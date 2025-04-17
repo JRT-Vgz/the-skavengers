4a3fcc47e69e72781523867a8662fbf356ae1706
Vargath <jrtvgz@gmail.com>
2025-04-17 18:23:46 +0200

    The Skavengers v1.1

--- The Skavengers/3-Data.Armory/ArmoryDbContextFactory.cs
+++ The Skavengers/3-Data.Armory/ArmoryDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace _3_Data.Armory
         public ArmoryDbContext CreateDbContext(string[] args)
         {
             //var connectionString = "Encripted string";
-            var connectionString = "poVoGDbfQD6iXgUbZLyAkAeBw13dOa+274xt6kRTTwDO2nKy4WB4k81O1mmIOQK8buBs6D9HBggtnEt+BFjUbDHzcCFFA8We5L8w1H/ae4xFlbEihwFfAnhCrcO1ONM8kx6aH130+gZ00YjTLRH4C5KPN3erQPw+zNBG54qlCIOgs/8S9VDkzYc7PD9Fb618";
+            var connectionString = "";
 
             var optionsBuilder = new DbContextOptionsBuilder<ArmoryDbContext>();
             optionsBuilder.UseSqlServer(DBEncrypter.Decrypt(connectionString));
